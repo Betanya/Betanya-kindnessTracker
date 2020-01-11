847d5f71fb3de30f8c4d8ea7bb6189b66f5fb43f
maxmart97 <maxmart97@gmail.com>
2020-01-11 17:57:10 -0600

    Attempting to connect to DynamoDB

--- src/routing/userRouter.js
+++ src/routing/userRouter.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
+var database = require("../data/database.js");
 
 router.get('', function (req, res) {
     res.render("user", { title: "User", userProfile: { nickname: "Max" } });
@@ -16,7 +17,21 @@ router.post('', (req, res) => {
         password: password
     };
 
-    res.render("user", { title: "User", userProfile: { nickname: username } });
+    let verification = database.verifyUserLogin(username, password);
+
+    console.log(verification);
+
+    if (verification === true) {
+        user = database.getUser(user);
+        console.log(user);
+        res.render("user", { title: "User", user: user, message: "" });
+    } else if (verification === false) {
+        res.render("signup", { title: "Signup", message: "Please sign up."});
+    } else if (verification === 'Invalid Password') {
+        res.render("index", { title: "Index", message: "Invalid Password" });
+    } else {
+        res.render("index", { title: "Index", message: verification });
+    }
 });
 
 module.exports = router;
